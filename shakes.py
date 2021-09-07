ad1231260b7110cac99c9ccdb87a11852259f42b
jpw <jon@mdi.eu.com>
2021-09-07 23:14:07 +0100

    ready to try new config

--- shakes.py
+++ shakes.py
@@ -3,4 +3,5 @@ from aitextgen import aitextgen
 ai = aitextgen(model_folder="models/shakes_model",
                 tokenizer_file="models/shakes.tokenizer.json")
 
-ai.generate(n=3, max_length=200)
+ai.generate(n=1, max_length=200, temperature=1.7)
+ai.generate_to_file(n=10, max_length=200, temperature=1.7)
